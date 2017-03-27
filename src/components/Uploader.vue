3a0636dadc3d03d6692a220e5be6af315d209cdf
ivan <qwIvan@users.noreply.github.com>
2017-03-27 13:22:52 +0800

    torrent upload

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,32 +1,50 @@
 <template>
   <div>
-    <q-uploader name="torrent" ref="uploader" @add="add" :url="url" hide-upload-button :labels="labels"></q-uploader>
+    <q-uploader name="torrent" ref="uploader" @upload="upload" @add="add" @start="start" @finish="finish"
+                :url="url"
+                hide-upload-button :labels="labels"/>
     <q-tooltip max-height="500px" anchor="top left">上传torrent</q-tooltip>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      labels: {
-        add: '<i>cloud_upload</i>'
+  import {Loading} from 'quasar'
+  export default {
+    data () {
+      return {
+        labels: {
+          add: '<i>cloud_upload</i>'
+        },
+        url: 'http://libivan.com:8888/torrent',
+      }
+    },
+    methods: {
+      add (files) {
+        let uploader = this.$refs.uploader
+        uploader.files = uploader.files.concat(files)
+        uploader.upload()
       },
-      url: 'libivan.com:8888/torrent'
-    }
-  },
-  methods: {
-    add: function (files) {
-      let uploader = this.$refs.uploader
-      uploader.files = uploader.files.concat(files)
-      uploader.upload()
+      start () {
+        this.$refs.uploader.uploading = false
+        Loading.show({
+          spinnerSize: 250,
+          spinner: 'hourglass'
+        })
+      },
+      finish () {
+//        this.$refs.uploader.$refs.file.value = ''  # PR #484 resolve it
+        Loading.hide()
+      },
+      upload(name, response){
+        // PR #483
+        console.log(response)
+      }
     }
   }
-}
 </script>
 
 <style>
-i {
-  animation: inherit!important
-}
+  i {
+    animation: inherit !important
+  }
 </style>
